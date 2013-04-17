2cfa387971cfe0948844f035455fea2eccafaa18
kenji.tazawa <kenji.tazawa@mixi.co.jp>
2013-04-17 20:55:43 +0900

    Add layout xml for fundamental 1st assignments2

--- assignments/fundamentals/1st/LayoutAssignment2/src/jp/mixi/assignment/layout/med/MainActivity.java
+++ assignments/fundamentals/1st/LayoutAssignment2/src/jp/mixi/assignment/layout/med/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.assignment_2_1);
     }
 
     @Override
