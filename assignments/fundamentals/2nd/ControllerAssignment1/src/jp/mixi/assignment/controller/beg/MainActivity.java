e1255f58fc9073e0a11c4f3bfd8e79725d2dc802
KeithYokoma <Keith.Yokoma@gmail.com>
2013-04-22 10:59:13 +0900

    Add activity practice session 2

--- assignments/fundamentals/2nd/ControllerAssignment1/src/jp/mixi/assignment/controller/beg/MainActivity.java
+++ assignments/fundamentals/2nd/ControllerAssignment1/src/jp/mixi/assignment/controller/beg/MainActivity.java
@@ -5,6 +5,14 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+/**
+ * TODO: 課題1
+ * {@link Toast#makeText(Context, CharSequence, int)} または
+ * {@link Toast#makeText(Context, int, int)} を利用して、各ライフサイクルメソッドがどのような順番で
+ * 実行されているかを確認してください。
+ * 確認したら、assignments/fundamentals/2nd/Report.md にその順番を記述してください。
+ * @author keishin.yokomaku
+ */
 public class MainActivity extends Activity {
 
     @Override
@@ -19,5 +27,4 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
 }
--- assignments/fundamentals/2nd/ControllerAssignment2/src/jp/mixi/assignment/controller/med/MainActivity.java
+++ assignments/fundamentals/2nd/ControllerAssignment2/src/jp/mixi/assignment/controller/med/MainActivity.java
@@ -1,23 +1,15 @@
 
 package jp.mixi.assignment.controller.med;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, SubActivity.class));
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
 }
