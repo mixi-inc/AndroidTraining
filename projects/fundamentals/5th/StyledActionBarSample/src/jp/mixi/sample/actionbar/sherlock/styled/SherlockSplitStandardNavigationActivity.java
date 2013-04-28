a9f694946c14c6d096280d0c195e16fd518091d0
KeithYokoma <Keith.Yokoma@gmail.com>
2013-04-28 15:59:47 +0900

    Create styled actionbar sample

---
+++ projects/fundamentals/5th/StyledActionBarSample/src/jp/mixi/sample/actionbar/sherlock/styled/SherlockSplitStandardNavigationActivity.java
@@ -0,0 +1,26 @@
+
+package jp.mixi.sample.actionbar.sherlock.styled;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+public class SherlockSplitStandardNavigationActivity extends SherlockActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+
+        // スタンダードナビゲーションモード
+        getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        // Menu が Android 標準 API のものとは違うので、区別するために getSupportMenuInflater() を呼ぶ
+        getSupportMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
---
+++ projects/fundamentals/5th/StyledActionBarSample/src/jp/mixi/sample/actionbar/sherlock/styled/SherlockTabNavigationActivity.java
@@ -0,0 +1,52 @@
+package jp.mixi.sample.actionbar.sherlock.styled;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.TabListener;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+
+public class SherlockTabNavigationActivity extends SherlockActivity implements TabListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+
+        // タブナビゲーションモードに設定
+        getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        // タブを作成して追加。タブの選択・解除・再選択をハンドリングするコールバックの TabListener をセットしないと実行時例外でクラッシュする
+        getSupportActionBar().addTab(getSupportActionBar().newTab().setText("Tab1").setTabListener(this));
+        getSupportActionBar().addTab(getSupportActionBar().newTab().setText("Tab2").setTabListener(this));
+        getSupportActionBar().addTab(getSupportActionBar().newTab().setText("Tab3").setTabListener(this));
+    }
+
+    // タブナビゲーションの Tab が選択された時のコールバック
+    @Override
+    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+        
+    }
+
+    // タブナビゲーションの Tab が選択解除された時のコールバック
+    @Override
+    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+        
+    }
+
+    // タブナビゲーションの Tab が再度選択された時のコールバック
+    @Override
+    public void onTabReselected(Tab tab, FragmentTransaction ft) {
+        
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        // Menu が Android 標準 API のものとは違うので、区別するために getSupportMenuInflater() を呼ぶ
+        getSupportMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
