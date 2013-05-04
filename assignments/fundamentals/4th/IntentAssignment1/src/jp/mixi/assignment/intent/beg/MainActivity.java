f7e04d6caebf01ad8e5d9d5c88e79782d1a21cee
KeithYokoma <Keith.Yokoma@gmail.com>
2013-05-04 19:55:39 +0900

    Add assignment for intents

---
+++ assignments/fundamentals/4th/IntentAssignment1/src/jp/mixi/assignment/intent/beg/MainActivity.java
@@ -0,0 +1,22 @@
+
+package jp.mixi.assignment.intent.beg;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View button = findViewById(R.id.ViewMixi);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO http://mixi.jp をブラウザで開くための Intent を作って、ブラウザを立ち上げる
+            }
+        });
+    }
+}
