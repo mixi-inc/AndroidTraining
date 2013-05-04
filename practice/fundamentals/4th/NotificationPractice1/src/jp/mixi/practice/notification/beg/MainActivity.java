ce3795770a437920429fdac7efbb6e2385c0c2d6
KeithYokoma <Keith.Yokoma@gmail.com>
2013-05-04 19:33:24 +0900

    Add practice session for the first notification

---
+++ practice/fundamentals/4th/NotificationPractice1/src/jp/mixi/practice/notification/beg/MainActivity.java
@@ -0,0 +1,16 @@
+
+package jp.mixi.practice.notification.beg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // TODO ここで通知を表示する
+    }
+}
