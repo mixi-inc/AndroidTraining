ed47ef44bf63d5587628a37593a508c7f9c7bf58
kenji.tazawa <kenji.tazawa@mixi.co.jp>
2013-05-07 18:52:40 +0900

    Add ContextMenu to ListView assignment

--- assignments/fundamentals/6th/ListViewAssignment/src/jp/mixi/assignment/listview/beg/MainActivity.java
+++ assignments/fundamentals/6th/ListViewAssignment/src/jp/mixi/assignment/listview/beg/MainActivity.java
@@ -3,7 +3,13 @@ package jp.mixi.assignment.listview.beg;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -11,6 +17,8 @@ import java.util.ArrayList;
 public class MainActivity extends Activity {
 
     private Activity mActivity;
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +38,16 @@ public class MainActivity extends Activity {
 
         // BookArrayAdapter bookArrayAdapter = new BookArrayAdapter(mActivity,
         // list);
+        mListView = (ListView) findViewById(R.id.BookList);
 
-        ListView listView = (ListView) findViewById(R.id.BookList);
         // TODO:ListViewにBookArrayAdapterをセットしてください。
         // TODO:ListViewをタップしたとき、BookActivityに遷移するようにしてください。遷移するときにBookクラスのtitleを渡してください。
         // (BookActivityは用意されているものを使用してください)
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerForContextMenu(mListView);
     }
 
     @Override
@@ -43,6 +55,48 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View view, ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, view, menuInfo);
+        // ContextMenuを設定
+        getMenuInflater().inflate(R.menu.context_menu_main, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        if (item.getItemId() != R.id.DeleteListItem && item.getItemId() != R.id.AddListItem) {
+            return false;
+        }
+
+        // MenuItemからContextMenuInfoを取得し、AdapterContextMenuInfoにキャストします
+        ContextMenuInfo menuInfo = item.getMenuInfo();
+        AdapterContextMenuInfo adapterInfo = (AdapterContextMenuInfo) menuInfo;
+
+        // AdapterContextMenuInfoから長押ししたリストアイテムのpositionを取得します
+        int position = adapterInfo.position;
+
+        // ListViewから長押しされたリストアイテムを取得します
+        Book book = (Book) mListView.getItemAtPosition(position);
+        // ListViewからセットされているAdapterを取得します
+        BookArrayAdapter adapter = (BookArrayAdapter) mListView.getAdapter();
+
+        if (item.getItemId() == R.id.DeleteListItem) {
+            // TODO:Adapterを使用して長押ししたデータを削除してください
+        } else if (item.getItemId() == R.id.AddListItem) {
+            // TODO:Adapterを使用して長押ししたデータを追加してください
+        }
+        // TODO:Adapterを使用して表示されているデータを更新してください
+        adapter.notifyDataSetChanged();
+        return true;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterForContextMenu(mListView);
     }
 
 }
