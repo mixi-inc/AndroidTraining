0491b44bfda79ae60466490113d392b188dcab1f
kenji.tazawa <kenji.tazawa@mixi.co.jp>
2013-05-23 17:42:52 +0900

    Add advanced 3rd project for include and merge

---
+++ projects/advanced/3rd/IncludeMergeSample/src/jp/mixi/sample/includemerge/MainActivity.java
@@ -0,0 +1,24 @@
+
+package jp.mixi.sample.includemerge;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.include_main);
+        // setContentView(R.layout.merge_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+}
