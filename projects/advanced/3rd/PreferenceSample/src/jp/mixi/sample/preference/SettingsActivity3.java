ffe583914e148b49e7868ef0be479965dbe3958a
kenji.tazawa <kenji.tazawa@mixi.co.jp>
2013-05-27 09:25:26 +0900

    Fix preference callback sample

--- projects/advanced/3rd/PreferenceSample/src/jp/mixi/sample/preference/SettingsActivity3.java
+++ projects/advanced/3rd/PreferenceSample/src/jp/mixi/sample/preference/SettingsActivity3.java
@@ -4,14 +4,15 @@ package jp.mixi.sample.preference;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
-import android.preference.Preference;
+import android.preference.EditTextPreference;
 import android.preference.PreferenceActivity;
 import android.view.Menu;
 
 /**
  * 設定変更時のコールバックサンプルです。
  */
-public class SettingsActivity3 extends PreferenceActivity implements OnSharedPreferenceChangeListener {
+public class SettingsActivity3 extends PreferenceActivity implements
+        OnSharedPreferenceChangeListener {
 
     // EditTextPreference で使用しているkey
     private static final String EDIT_TEXT_KEY = "edit_texit_key";
@@ -28,27 +29,25 @@ public class SettingsActivity3 extends PreferenceActivity implements OnSharedPre
         // 処理対象のPreferenceかをkeyで判定
         if (key.equals(EDIT_TEXT_KEY)) {
             // keyからPreferenceのインスタンスを取得
-            Preference connectionPref = findPreference(EDIT_TEXT_KEY);
+            EditTextPreference editPref = (EditTextPreference) findPreference(EDIT_TEXT_KEY);
             // Preferenceのサマリーを変更
-            connectionPref.setSummary(sharedPreferences.getString(EDIT_TEXT_KEY, ""));
+            editPref.setSummary(editPref.getText());
         }
-
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         // リスナーを登録
-        getPreferenceScreen().getSharedPreferences()
-                .registerOnSharedPreferenceChangeListener(this);
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         // リスナーを登録解除
-        getPreferenceScreen().getSharedPreferences()
-                .unregisterOnSharedPreferenceChangeListener(this);
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(
+                this);
     }
 
     @Override
