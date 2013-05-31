fe42184b638b96f0cac2caae35a62fb4348def47
cafedeaqua <cafedeaqua@gmail.com>
2013-05-31 10:30:01 +0900

    replace fragment.

--- assignments/fundamentals/2nd/ControllerAssignment3/src/jp/mixi/assignment/controller/adv/MainActivity.java
+++ assignments/fundamentals/2nd/ControllerAssignment3/src/jp/mixi/assignment/controller/adv/MainActivity.java
@@ -1,20 +1,20 @@
 
 package jp.mixi.assignment.controller.adv;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.os.*;
+import android.support.v4.app.*;
+import android.view.*;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(final Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
