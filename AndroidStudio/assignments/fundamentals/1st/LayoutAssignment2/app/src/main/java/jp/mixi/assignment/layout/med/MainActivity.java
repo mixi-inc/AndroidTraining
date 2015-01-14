1c3242a0d32ffda25ef7fcb70f66647d3a0c7fb7
Hideyuki Kikuma <hideyuki.kikuma@gmail.com>
2015-01-15 01:51:06 +0900

    migrate Eclipse to AndroidStudio

---
+++ AndroidStudio/assignments/fundamentals/1st/LayoutAssignment2/app/src/main/java/jp/mixi/assignment/layout/med/MainActivity.java
@@ -0,0 +1,23 @@
+
+package jp.mixi.assignment.layout.med;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.assignment_2_1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+}
