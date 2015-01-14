263ec7dc4977cf85db054d44ca4d88f0880faabb
Hideyuki Kikuma <hideyuki.kikuma@gmail.com>
2015-01-15 02:00:34 +0900

    migrate Eclipse to AndroidStudio

---
+++ AndroidStudio/assignments/fundamentals/4th/NotificationAssignment1/app/src/main/java/jp/mixi/assignment/notification/beg/MainActivity.java
@@ -0,0 +1,15 @@
+
+package jp.mixi.assignment.notification.beg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // TODO ここで通知を表示する
+    }
+}
