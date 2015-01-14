c6fb2af3e8e2d35f7a9dbe95759d6076d2ec6f5a
Hideyuki Kikuma <hideyuki.kikuma@gmail.com>
2015-01-15 02:06:09 +0900

    migrate Eclipse to AndroidStudio

---
+++ AndroidStudio/assignments/fundamentals/7th/SerializableAssignment/app/src/main/java/jp/mixi/assignment/serializable/beg/DetailActivity.java
@@ -0,0 +1,22 @@
+package jp.mixi.assignment.serializable.beg;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class DetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+}
