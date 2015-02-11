e96d42bff3f45eeefaa894fc9ebcb05a5cc5fcaa
Suino <violet1101@gmail.com>
2015-02-12 00:33:28 +0900

    add practice animation answer

--- AndroidStudio/answers/practice/fundamentals/3rd/ResourceManagement/app/src/main/java/jp/mixi/practice/resourcemanagement/AnimationPracticeActivity.java
+++ AndroidStudio/answers/practice/fundamentals/3rd/ResourceManagement/app/src/main/java/jp/mixi/practice/resourcemanagement/AnimationPracticeActivity.java
@@ -2,6 +2,9 @@ package jp.mixi.practice.resourcemanagement;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 /**
  * Created by takafumi.nanao on 2015/02/05.
@@ -11,5 +14,9 @@ public class AnimationPracticeActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.animation_practice);
+
+        View animationView = findViewById(R.id.animation_view);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.practice);
+        animationView.startAnimation(animation);
     }
 }
