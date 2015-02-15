69599bf4246edddf660580a032f700e85032277c
Suino <violet1101@gmail.com>
2015-02-15 22:16:43 +0900

    copy projects to answer directory

---
+++ AndroidStudio/answers/assignments/fundamentals/3rd/StringResourceAssignment1/app/src/main/java/jp/mixi/assignment/res/string/beg/MainActivity.java
@@ -0,0 +1,16 @@
+
+package jp.mixi.assignment.res.string.beg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // TODO 1 個のものと、複数個のものの両方を並べて表示するため、両方用の TextView を取り出し、リソースへアクセスして表示する
+    }
+}
