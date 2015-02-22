d6e82c65eb26a40b4651334f06e5dac8fb72e212
Suino <violet1101@gmail.com>
2015-02-22 11:41:30 +0900

    add notification practice

---
+++ AndroidStudio/answers/practice/fundamentals/4th/NotificationPractice1/app/src/main/java/jp/mixi/practice/notification/beg/SubActivity.java
@@ -0,0 +1,15 @@
+
+package jp.mixi.practice.notification.beg;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SubActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+    }
+}
