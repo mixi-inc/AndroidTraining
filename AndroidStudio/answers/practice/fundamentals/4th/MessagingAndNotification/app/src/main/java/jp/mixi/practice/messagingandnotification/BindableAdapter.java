fadaaf27f8b5cf2ff83b1891f3b69c05f755860e
Suino <violet1101@gmail.com>
2015-02-26 00:25:59 +0900

    解答プロジェクト追加

---
+++ AndroidStudio/answers/practice/fundamentals/4th/MessagingAndNotification/app/src/main/java/jp/mixi/practice/messagingandnotification/BindableAdapter.java
@@ -0,0 +1,38 @@
+package jp.mixi.practice.messagingandnotification;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+/**
+ * Created by Hideyuki.Kikuma on 2015/02/18.
+ */
+public abstract class BindableAdapter<T> extends ArrayAdapter<T> {
+    private LayoutInflater mInflater;
+
+    public BindableAdapter(Context context, List<T> items) {
+        super(context, 0, items);
+        mInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public final View getView(int position, View view, ViewGroup container) {
+        if (view == null) {
+            view = newView(mInflater, position, container);
+            if (view == null) {
+                throw new IllegalStateException("newView result must not be null.");
+            }
+        }
+        bindView(getItem(position), position, view);
+        return view;
+    }
+
+    public abstract View newView(LayoutInflater inflater, int position, ViewGroup container);
+
+    public abstract void bindView(T item, int position, View view);
+
+}
---
+++ AndroidStudio/answers/practice/fundamentals/4th/MessagingAndNotification/app/src/main/java/jp/mixi/practice/messagingandnotification/NotificationActivity.java
@@ -0,0 +1,18 @@
+package jp.mixi.practice.messagingandnotification;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by suino on 2015/02/26.
+ */
+public class NotificationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+
+        // TODO ここで通知を表示する
+    }
+}
