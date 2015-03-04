fbbf71faf809f4594900c6455d660d0e57def4ba
Suino <violet1101@gmail.com>
2015-03-05 00:03:54 +0900

    ちゃんとcommitできてなかった......

---
+++ AndroidStudio/assignments/fundamentals/4th/MessagingAndNotification/app/src/main/java/jp/mixi/assignment/messagingandnotification/NotificationActivity1.java
@@ -0,0 +1,18 @@
+package jp.mixi.assignment.messagingandnotification;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by suino on 2015/02/26.
+ */
+public class NotificationActivity1 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification1);
+
+        // TODO ここで通知を表示する
+    }
+}
---
+++ AndroidStudio/assignments/fundamentals/4th/MessagingAndNotification/app/src/main/java/jp/mixi/assignment/messagingandnotification/NotificationSubActivity1.java
@@ -0,0 +1,20 @@
+package jp.mixi.assignment.messagingandnotification;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by suino on 2015/02/26.
+ */
+public class NotificationSubActivity1 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_sub);
+
+        TextView view = (TextView) findViewById(R.id.TextView);
+        view.setText(getString(R.string.sub_activity, 1));
+    }
+}
