cd824e197d875e4e53dbe6f86b0757e1ca90054a
Suino <violet1101@gmail.com>
2015-03-04 23:13:14 +0900

    fix assignment1

--- AndroidStudio/assignments/fundamentals/4th/MessagingAndNotification/app/src/main/java/jp/mixi/practice/messagingandnotification/IntentActivity1.java
+++ AndroidStudio/assignments/fundamentals/4th/MessagingAndNotification/app/src/main/java/jp/mixi/practice/messagingandnotification/IntentActivity1.java
@@ -13,31 +13,12 @@ public class IntentActivity1 extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent_1);
 
-        View button1 = findViewById(R.id.CallActivity1);
-        View button2 = findViewById(R.id.CallActivity2);
-        View button3 = findViewById(R.id.CallActivity3);
+        View button = findViewById(R.id.ViewMixi);
 
-        button1.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO ここに、NewActivity1 を呼び出す処理を書く
-
-            }
-        });
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO ここに、NewActivity2 を呼び出す処理を書く
-                // TODO NewActivity2 は、toast_message をキーとした Extra のデータを必要としているので、適宜 Intent に含めること
-
-            }
-        });
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO ここに、NewActivity3 を呼び出す処理を書く
-                // TODO Intent に、Intent.FLAG_ACTIVITY_NO_HISTORY という flag をセットするとどうなるかレポートすること
-
+                // TODO http://mixi.jp をブラウザで開くための Intent を作って、ブラウザを立ち上げる
             }
         });
     }
