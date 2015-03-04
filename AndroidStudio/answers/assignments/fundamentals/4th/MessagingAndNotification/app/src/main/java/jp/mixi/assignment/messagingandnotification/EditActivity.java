833f849780237c7125be3482548a020352d4e6ef
Suino <violet1101@gmail.com>
2015-03-05 00:47:55 +0900

    add intent2 answer

--- AndroidStudio/answers/assignments/fundamentals/4th/MessagingAndNotification/app/src/main/java/jp/mixi/assignment/messagingandnotification/EditActivity.java
+++ AndroidStudio/answers/assignments/fundamentals/4th/MessagingAndNotification/app/src/main/java/jp/mixi/assignment/messagingandnotification/EditActivity.java
@@ -1,13 +1,17 @@
 package jp.mixi.assignment.messagingandnotification;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 /**
  * Created by suino on 2015/03/04.
  */
 public class EditActivity extends Activity {
+    public static final int REQUEST_CODE = 1;
+    public static final String KEY_TEXT = "text";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +19,16 @@ public class EditActivity extends Activity {
         setContentView(R.layout.activity_edit);
 
         View button = findViewById(R.id.Submit);
+        final EditText editText = (EditText) findViewById(R.id.MyEdit);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO ここで、id が MyEdit の EditText からテキストを取得して、結果にセットする
+                String text = editText.getText().toString();
+                Intent intent = new Intent();
+                intent.putExtra(KEY_TEXT, text);
+                setResult(REQUEST_CODE, intent);
+                finish(); // 呼び出し元に戻る
             }
         });
     }
--- AndroidStudio/answers/assignments/fundamentals/4th/MessagingAndNotification/app/src/main/java/jp/mixi/assignment/messagingandnotification/IntentActivity2.java
+++ AndroidStudio/answers/assignments/fundamentals/4th/MessagingAndNotification/app/src/main/java/jp/mixi/assignment/messagingandnotification/IntentActivity2.java
@@ -1,8 +1,10 @@
 package jp.mixi.assignment.messagingandnotification;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by suino on 2015/02/25.
@@ -19,9 +21,22 @@ public class IntentActivity2 extends Activity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO EditText と ボタンを 1 つずつ持つ新しい Activity を呼び出し、ボタンを押した時に結果を返すように実装する
-                // TODO 返ってきた結果を Toast で表示するところも実装すること
+                Intent intent = new Intent(IntentActivity2.this, EditActivity.class);
+                startActivityForResult(intent, EditActivity.REQUEST_CODE);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case EditActivity.REQUEST_CODE:
+                Toast.makeText(this, data.getStringExtra(EditActivity.KEY_TEXT), Toast.LENGTH_LONG).show();
+                break;
+            default:
+                break;
+        }
     }
 }
