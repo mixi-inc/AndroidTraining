6a136f7550674448b13e8b722dbe9f7cc6c9e622
Suino <violet1101@gmail.com>
2015-03-05 01:57:31 +0900

    add notification2 answer

--- AndroidStudio/answers/assignments/fundamentals/4th/MessagingAndNotification/app/src/main/java/jp/mixi/assignment/messagingandnotification/NotificationActivity2.java
+++ AndroidStudio/answers/assignments/fundamentals/4th/MessagingAndNotification/app/src/main/java/jp/mixi/assignment/messagingandnotification/NotificationActivity2.java
@@ -1,21 +1,38 @@
 package jp.mixi.assignment.messagingandnotification;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 
 /**
  * Created by suino on 2015/02/26.
  */
 public class NotificationActivity2 extends Activity {
-    public static final String ACITON_VIEW_MY_OWN = "jp.mixi.assignment.messagingandnotification.android.intent.action.VIEW_MY_OWN";
+    public static final String ACTION_VIEW_MY_OWN = "jp.mixi.assignment.messagingandnotification.android.intent.action.VIEW_MY_OWN";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification2);
 
-        // TODO ここで通知を表示する
-        // TODO intent には、ACTION_VIEW_MY_OWN の action をセットするだけにしておく
-        // TODO 通知をタップした時に、複数の選択肢が表示されるが、どれがどの Activity に対応しているかがわかりづらくなっているので、AndroidManifest を編集して表示をわかりやすくする
+        Intent intent = new Intent();
+        intent.setAction(ACTION_VIEW_MY_OWN);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        Notification notification = builder
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle("通知テスト")
+                .setContentText("通知の詳細テスト")
+                .setContentIntent(pendingIntent)
+                .build();
+
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0, notification);
     }
 }
