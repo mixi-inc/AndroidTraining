29ed1967c24f1ea4c8ff6d05725fbf6e50fa5bea
Suino <violet1101@gmail.com>
2015-03-04 23:27:02 +0900

    fix assignment2

--- AndroidStudio/assignments/fundamentals/4th/MessagingAndNotification/app/src/main/java/jp/mixi/practice/messagingandnotification/IntentActivity2.java
+++ AndroidStudio/assignments/fundamentals/4th/MessagingAndNotification/app/src/main/java/jp/mixi/practice/messagingandnotification/IntentActivity2.java
@@ -9,39 +9,18 @@ import android.view.View;
  */
 public class IntentActivity2 extends Activity {
 
-    public static final String ACTION_FIRST = "jp.mixi.practice.messagingandnotification.intent.action.FIRST";
-    public static final String ACTION_SECOND = "jp.mixi.practice.messagingandnotification.intent.action.SECOND";
-    public static final String ACTION_THIRD = "jp.mixi.practice.messagingandnotification.intent.action.THIRD";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent_2);
 
-        View button1 = findViewById(R.id.CallAction1);
-        View button2 = findViewById(R.id.CallAction2);
-        View button3 = findViewById(R.id.CallAction3);
+        View button = findViewById(R.id.CallActivityForResult);
 
-        // TODO それぞれ、Broadcast を受け取ったら Log.v(String, String) を利用して、ログ出力にどの Action を受信したかを表示する処理を書くこと
-        button1.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO ここに、ACTION_FIRST を呼び出す処理を書く
-
-            }
-        });
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO ここに、ACTION_SECOND を呼び出す処理を書く
-
-            }
-        });
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO ここに、ACTION_THIRD を呼び出す処理を書く
-
+                // TODO EditText と ボタンを 1 つずつ持つ新しい Activity を呼び出し、ボタンを押した時に結果を返すように実装する
+                // TODO 返ってきた結果を Toast で表示するところも実装すること
             }
         });
     }
