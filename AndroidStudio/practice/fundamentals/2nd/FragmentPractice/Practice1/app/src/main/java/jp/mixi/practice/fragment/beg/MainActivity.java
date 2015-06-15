c0cdb5d0278874ba4d7df064278dc5e02b900ba6
Nguyen Khanh Linh <linh.nguyenkhanh@gmo-media.jp>
2015-06-15 15:41:38 +0900

    Built layout

--- AndroidStudio/practice/fundamentals/2nd/FragmentPractice/Practice1/app/src/main/java/jp/mixi/practice/fragment/beg/MainActivity.java
+++ AndroidStudio/practice/fundamentals/2nd/FragmentPractice/Practice1/app/src/main/java/jp/mixi/practice/fragment/beg/MainActivity.java
@@ -1,17 +1,31 @@
 
 package jp.mixi.practice.fragment.beg;
 
-import android.os.Bundle;
+
 import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.add(R.id.fragmentContainer, BlankFragment.newInstance("",""));
+        fragmentTransaction.replace(R.id.fragmentContainer, BlankFragment.newInstance("",""));
+        fragmentTransaction.commit();
+
+
+
+ }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- AndroidStudio/practice/fundamentals/5th/ActionBarPractice1/app/src/main/java/jp/mixi/practice/actionbar/beg/MainActivity.java
+++ AndroidStudio/practice/fundamentals/5th/ActionBarPractice1/app/src/main/java/jp/mixi/practice/actionbar/beg/MainActivity.java
@@ -1,23 +1,81 @@
 
 package jp.mixi.practice.actionbar.beg;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class MainActivity extends Activity {
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.OnNavigationListener;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+
+import com.actionbarsherlock.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends SherlockActivity implements OnNavigationListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> list = new ArrayList<String>();
+        list.add("Menu1");
+        list.add("Menu2");
+        list.add("Menu3");
+
+        getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+        getSupportActionBar().setListNavigationCallbacks(
+                new ArrayAdapter<String>(getApplicationContext(),
+                        android.R.layout.simple_list_item_1,
+                        android.R.id.text1, list),
+                this);
+
+        getSupportActionBar().setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
+
+
+        Button button = (Button)findViewById(R.id.goBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,TabMenuActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getSupportMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    // List Navigation の一覧から項目を選択したら呼び出されるコールバック
+    @Override
+    public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+        return false;
+
     }
 
 }
