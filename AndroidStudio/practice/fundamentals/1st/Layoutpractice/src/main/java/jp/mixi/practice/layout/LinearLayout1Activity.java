f2a77a1ae2ca63efb1150713c2e978ecfa072340
Hideyuki Kikuma <hideyuki.kikuma@gmail.com>
2018-03-16 00:55:19 +0900

    Add LayoutPractice

---
+++ AndroidStudio/practice/fundamentals/1st/Layoutpractice/src/main/java/jp/mixi/practice/layout/LinearLayout1Activity.java
@@ -0,0 +1,13 @@
+package jp.mixi.practice.layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LinearLayout1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout1);
+    }
+}
