0626e69aeacafe68290ced3e8ac492f5fee5c8dc
Hideyuki Kikuma <hideyuki.kikuma@gmail.com>
2018-03-18 23:39:19 +0900

    Add LayoutAssignment

---
+++ AndroidStudio/assignments/fundamentals/1st/LayoutAssignment/src/main/java/jp/mixi/assignment/layout/MainActivity.java
@@ -0,0 +1,16 @@
+package jp.mixi.assignment.layout;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
