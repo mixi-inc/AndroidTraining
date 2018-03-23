984d066a15d735059334797a32ef0176f55d8493
Hideyuki Kikuma <hideyuki.kikuma@gmail.com>
2018-03-24 02:31:46 +0900

    Add SharedPreferencesActivity

--- AndroidStudio/practice/fundamentals/7th/SerializeAndCollectionPractice/src/main/java/jp/mixi/practice/serializeandcollection/MainActivity.java
+++ AndroidStudio/practice/fundamentals/7th/SerializeAndCollectionPractice/src/main/java/jp/mixi/practice/serializeandcollection/MainActivity.java
@@ -19,5 +19,12 @@ public class MainActivity extends AppCompatActivity {
                 context.startActivity(new Intent(context, SerializableActivity.class));
             }
         });
+        findViewById(R.id.shared_preferences_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = view.getContext();
+                context.startActivity(new Intent(context, SharedPreferencesActivity.class));
+            }
+        });
     }
 }
