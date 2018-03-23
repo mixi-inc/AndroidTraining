77af8bf0d3e08e01cfdf624f49608434d50c0d1b
Hideyuki Kikuma <hideyuki.kikuma@gmail.com>
2018-03-24 03:19:18 +0900

    Add SerializableAssignment

--- AndroidStudio/assignments/fundamentals/7th/SerializeAndCollectionAssignment/src/main/java/jp/mixi/assignment/serializeandcollection/MainActivity.java
+++ AndroidStudio/assignments/fundamentals/7th/SerializeAndCollectionAssignment/src/main/java/jp/mixi/assignment/serializeandcollection/MainActivity.java
@@ -1,7 +1,10 @@
 package jp.mixi.assignment.serializeandcollection;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.serializable_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = view.getContext();
+                context.startActivity(new Intent(context, SerializableListActivity.class));
+            }
+        });
     }
 }
---
+++ AndroidStudio/assignments/fundamentals/7th/SerializeAndCollectionAssignment/src/main/java/jp/mixi/assignment/serializeandcollection/SerializableDetailActivity.java
@@ -0,0 +1,13 @@
+package jp.mixi.assignment.serializeandcollection;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SerializableDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_serializable_detail);
+    }
+}
