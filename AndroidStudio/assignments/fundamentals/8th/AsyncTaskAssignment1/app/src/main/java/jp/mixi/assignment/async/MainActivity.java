da6cf404497d78ca4b5805205d046c8b46d32849
Hideyuki Kikuma <hideyuki.kikuma@gmail.com>
2018-03-24 23:34:45 +0900

    move package

--- AndroidStudio/assignments/fundamentals/8th/AsyncTaskAssignment1/app/src/main/java/jp/mixi/assignment/async/MainActivity.java
+++ AndroidStudio/assignments/fundamentals/8th/AsyncTaskAssignment1/app/src/main/java/jp/mixi/assignment/async/MainActivity.java
@@ -1,10 +1,10 @@
-package jp.mixi.assignment.async.beg;
+package jp.mixi.assignment.async;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- AndroidStudio/assignments/fundamentals/8th/AsyncTaskAssignment1/app/src/main/java/jp/mixi/assignment/async/PreferencesEntity.java
+++ AndroidStudio/assignments/fundamentals/8th/AsyncTaskAssignment1/app/src/main/java/jp/mixi/assignment/async/PreferencesEntity.java
@@ -1,4 +1,4 @@
-package jp.mixi.assignment.async.beg;
+package jp.mixi.assignment.async;
 
 import android.os.Parcel;
 import android.os.Parcelable;
