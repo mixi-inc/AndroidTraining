03ca244d21fb1c3f74ce50cabbe8f1fd78018b5b
Hideyuki Kikuma <hideyuki.kikuma@gmail.com>
2018-03-25 03:33:49 +0900

    Add AsyncTask practice

--- AndroidStudio/practice/fundamentals/8th/AsyncPractice/src/main/java/jp/mixi/practice/async/MainActivity.java
+++ AndroidStudio/practice/fundamentals/8th/AsyncPractice/src/main/java/jp/mixi/practice/async/MainActivity.java
@@ -26,5 +26,12 @@ public class MainActivity extends AppCompatActivity {
                 context.startActivity(new Intent(context, AsyncTaskLoaderActivity.class));
             }
         });
+        findViewById(R.id.async_task_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = view.getContext();
+                context.startActivity(new Intent(context, AsyncTaskActivity.class));
+            }
+        });
     }
 }
